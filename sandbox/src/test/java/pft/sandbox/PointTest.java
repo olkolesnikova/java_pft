5ca970594cfff16e4bba1e2b09f489419f283921
olkolesnikova <59109598+olkolesnikova@users.noreply.github.com>
2019-12-23 00:44:13 +0500

    Создан тест для проверки правильности расчета расстояния

---
+++ sandbox/src/test/java/pft/sandbox/PointTest.java
@@ -0,0 +1,17 @@
+package pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTest {
+
+    @Test
+
+    public void testLength() {
+
+        Point p1 = new Point(1, 4);
+        Point p2 = new Point(4, 8);
+        Assert.assertEquals(p1.distance(p2), 5.0);
+    }
+
+}
