f682cea14c94530d1cd0be37356ecb1f616d039a
olkolesnikova <59109598+olkolesnikova@users.noreply.github.com>
2019-12-22 20:02:11 +0500

    Функция Distance преобразована в метод

--- sandbox/src/main/java/pft/sandbox/Prosto.java
+++ sandbox/src/main/java/pft/sandbox/Prosto.java
@@ -4,17 +4,12 @@ public class Prosto{
 
    public static void main(String[] args){
 
-       Point p1 = new Point(2, 5);
+       Point p1 = new Point(2, 4);
 
-       Point p2 = new Point(4, 6);
+       Point p2 = new Point(3, 5);
 
-       System.out.println("Длина отрезка равна " + distance(p1, p2));
+       System.out.println("Длина отрезка равна " + p1.distance(p2));
    }
-
-    public static double distance(Point p1, Point p2){
-       return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
-
-    }
 
 
 
