146440d62a6f3710643db26d9d55cda046e3491e
olkolesnikova <59109598+olkolesnikova@users.noreply.github.com>
2019-12-22 15:29:44 +0500

    Добавлена функция distance

---
+++ sandbox/src/main/java/pft/sandbox/Point.java
@@ -0,0 +1,7 @@
+package pft.sandbox;
+
+public class Point {
+
+    public double x;
+    public double y;
+}
--- sandbox/src/main/java/pft/sandbox/Prosto.java
+++ sandbox/src/main/java/pft/sandbox/Prosto.java
@@ -3,7 +3,24 @@ package pft.sandbox;
 public class Prosto{
 
    public static void main(String[] args){
-   System.out.println("Java?");
+
+       Point p1 = new Point();
+       p1.x = 4;
+       p1.y = 6;
+
+       Point p2 = new Point();
+       p2.x = 3;
+       p2.y = 1;
+
+
+       System.out.println("Длина отрезка равна " + distance(p1, p2));
    }
+
+    public static double distance(Point p1, Point p2){
+       return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+
+    }
+
+
 
 }
