617035e5bcb385d0ee4375a0f0d8b63046468ad5
olkolesnikova <59109598+olkolesnikova@users.noreply.github.com>
2019-12-24 00:31:51 +0500

    На всякий случай

--- sandbox/build/scripts/sandbox.bat
+++ sandbox/build/scripts/sandbox.bat
@@ -79,7 +79,7 @@ set CMD_LINE_ARGS=%*
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\sandbox.jar
+set CLASSPATH=%APP_HOME%\lib\sandbox.jar;%APP_HOME%\lib\testng-6.9.10.jar;%APP_HOME%\lib\jcommander-1.48.jar;%APP_HOME%\lib\bsh-2.0b4.jar
 
 @rem Execute sandbox
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %SANDBOX_OPTS%  -classpath "%CLASSPATH%" Prosto %CMD_LINE_ARGS%
