881ce6e3447cf70b4392c6e02ad2d7f56f908252
olkolesnikova <59109598+olkolesnikova@users.noreply.github.com>
2020-01-07 17:29:49 +0500

    Реализована возможность запуска тестов в Chrome и Firefox

--- adressbook-web-tests/src/test/java/pft/adressbook/tests/TestBase.java
+++ adressbook-web-tests/src/test/java/pft/adressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package pft.adressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pft.adressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
