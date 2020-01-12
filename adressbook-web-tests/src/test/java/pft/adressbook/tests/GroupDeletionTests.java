c5d344285501ee4f937ccfd7edcdf067126ca232
olkolesnikova <59109598+olkolesnikova@users.noreply.github.com>
2020-01-12 15:12:55 +0500

    обеспечение предусловия существования для групп

--- adressbook-web-tests/src/test/java/pft/adressbook/tests/GroupDeletionTests.java
+++ adressbook-web-tests/src/test/java/pft/adressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package pft.adressbook.tests;
 
 import org.testng.annotations.Test;
+import pft.adressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+        app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups("delete");
     app.getGroupHelper().returnToGroupPage();
