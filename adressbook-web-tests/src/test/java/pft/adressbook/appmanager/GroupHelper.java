465395e95df08b5f6861b207172f21ee5b417015
olkolesnikova <59109598+olkolesnikova@users.noreply.github.com>
2020-01-26 14:42:48 +0500

    выбор элемента из списка по порядковому номеру в тестах для удаления и модификации групп

--- adressbook-web-tests/src/test/java/pft/adressbook/appmanager/GroupHelper.java
+++ adressbook-web-tests/src/test/java/pft/adressbook/appmanager/GroupHelper.java
@@ -36,9 +36,9 @@ public class GroupHelper extends HelperBase {
         click(By.name("delete"));
     }
 
-    public void selectGroup() {
+    public void selectGroup(int index) {
+        wd.findElements(By.name("selected[]")).get(index).click();
 
-        click(By.name("selected[]"));
     }
 
     public void initGroupModification() {
