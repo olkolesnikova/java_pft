21a27d4a33e0b4ca84e0c1b8262f21746e68f715
olkolesnikova <59109598+olkolesnikova@users.noreply.github.com>
2020-02-08 20:35:13 +0500

    провайдер тестовых данных

--- adressbook-web-tests/src/test/java/pft/adressbook/tests/ContactCreationTest.java
+++ adressbook-web-tests/src/test/java/pft/adressbook/tests/ContactCreationTest.java
@@ -1,10 +1,15 @@
 package pft.adressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pft.adressbook.model.ContactData;
 import pft.adressbook.model.Contacts;
+import pft.adressbook.model.GroupData;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -13,14 +18,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTest extends TestBase {
 
 
-    @Test
-  public void testContactCreation() throws Exception {
+    @DataProvider
+    public Iterator<Object[]> validContacts() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[]{new ContactData().withName("Петр").withFamily("Петров").withAddress("Москва").withGroup("test1")});
+        list.add(new Object[]{new ContactData().withName("Иван").withFamily("Иванов").withAddress("Екатеринбург").withGroup("test1")});
+        list.add(new Object[]{new ContactData().withName("Семен").withFamily("Семенов").withAddress("Новосибирск").withGroup("test1")});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) throws Exception {
         Contacts before = app.getContactHelper().all();
         app.getContactHelper().gotoNewContactPage();
         File photo = new File("src/test/resources/volk.jpg");
-        ContactData contact = new ContactData()
-                .withFamily("Соколов").withName("Олег").withAddress("Екатеринбург").withTelephone("4951251")
-                .withMobile("222").withWork("333").withEmail("sokolov85@mail.ru").withPhoto(photo).withGroup("test1");
+
         app.getContactHelper().fillContactForm(contact, true);
         app.getContactHelper().submitContactCreation();
         app.getContactHelper().returnToHomePage();
