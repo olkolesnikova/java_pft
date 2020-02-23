31abf48faa09d343f09d7d798a0eb3f988687983
olkolesnikova <59109598+olkolesnikova@users.noreply.github.com>
2020-02-23 12:37:48 +0500

    создан тест LoginTests

---
+++ mantis-tests/src/test/java/pft/mantis/tests/LoginTests.java
@@ -0,0 +1,20 @@
+package pft.mantis.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pft.mantis.appmanager.HttpSession;
+
+import java.io.IOException;
+
+import static org.testng.Assert.assertTrue;
+
+public class LoginTests extends TestBase {
+
+    @Test
+
+    public void testLogin() throws IOException {
+        HttpSession session = app.newSession();
+        assertTrue(session.login("administrator", "root"));
+        Assert.assertTrue(session.isLoggedInAs("administrator"));
+    }
+}
