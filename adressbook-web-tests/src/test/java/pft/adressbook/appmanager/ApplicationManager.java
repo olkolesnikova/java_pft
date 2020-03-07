5e6249d1f974c258839540d34612342f56c0e3b8
olkolesnikova <59109598+olkolesnikova@users.noreply.github.com>
2020-03-07 19:57:47 +0500

    1

--- adressbook-web-tests/src/test/java/pft/adressbook/appmanager/ApplicationManager.java
+++ adressbook-web-tests/src/test/java/pft/adressbook/appmanager/ApplicationManager.java
@@ -2,6 +2,7 @@ package pft.adressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -52,6 +53,7 @@ public class ApplicationManager {
         } else {
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setBrowserName(browser);
+            capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win10")));
             wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
         wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
